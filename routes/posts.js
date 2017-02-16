70fdb87d9ee9213fd7d96f4089fb67f0948161a0
Valentyn Zozulia <flydze@gmail.com>
2017-02-16 15:11:30 -0300

    - new: Add a form to create new posts - new: Implement post saving to the database - new: Implement showing categories from the database on the add new post page

---
+++ routes/posts.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+
+var router = express.Router();
+
+router.get('/add', function(req, res, next) {
+  var categories = db.get('categories');
+  categories.find({}, {}, function(err, categories) {
+    res.render('addpost', {
+      'title': 'Add Post',
+      'categories': categories
+    });
+  });
+});
+
+router.post('/add', upload.single('mainimage'), function(req, res, next) {
+  // get form values
+  var title = req.body.title
+    , category = req.body.category
+    , body = req.body.body
+    , author = req.body.author
+    , date = new Date()
+    , mainimage;
+
+  // check image upload
+  if (req.file) {
+    mainimage = req.file.filename;
+  } else {
+    mainimage = 'noimage.jpg';
+  }
+
+  // form validation
+  req.checkBody('title', 'Title field is required').notEmpty();
+  req.checkBody('body', 'Body field is required').notEmpty();
+
+  // check errors
+  var errors = req.validationErrors();
+  if (errors) {
+    res.render('addpost', {
+      'errors': errors
+    });
+  } else {
+    var posts = db.get('posts');
+    posts.insert({
+      "title": title,
+      "body": body,
+      "category": category,
+      "author": author,
+      "date": date,
+      "mainimage": mainimage
+    }, function(err, post) {
+      if (err) {
+        res.send(err);
+      } else {
+        req.flash('success', 'Post Added');
+        res.location('/');
+        res.redirect('/');
+      }
+
+    });
+  }
+});
+
+module.exports = router;
